a77d3d4795ff0d8237adf5106d2ea4b41cb271bb
Tomasz Burzy <Tomasz9797@live.co.uk>
2023-04-12 13:24:52 +0100

    First version of working site.

---
+++ Static/script.js
@@ -0,0 +1,144 @@
+const form = document.getElementById("add-form");
+const itemList = document.getElementById("item-list");
+
+// Add item to list
+function addItem(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const link = document.getElementById("link").value;
+  const image = document.getElementById("image").value;
+  const position = document.getElementById("position").value;
+
+  const item = { name, link, image, position };
+
+  fetch("/api/items", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(item),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const li = createItemElement(data);
+      itemList.appendChild(li);
+      form.reset();
+    })
+    .catch((error) => console.error(error));
+}
+
+// Edit item
+function editItem(item) {
+  const name = prompt("Enter a new name:", item.name);
+  const link = prompt("Enter a new link:", item.link);
+  const image = prompt("Enter a new image URL:", item.image);
+  const position = prompt("Enter a new position:", item.position);
+
+  const updatedItem = { ...item, name, link, image, position };
+
+  fetch(`/api/items/${item.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(updatedItem),
+  })
+    .then(() => {
+      const li = itemList.querySelector(`li[data-id="${item.id}"]`);
+      const updatedLi = createItemElement(updatedItem);
+      itemList.replaceChild(updatedLi, li);
+    })
+    .catch((error) => console.error(error));
+}
+
+// Create edit button
+function createEditButton(item) {
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "Edit";
+  editBtn.addEventListener("click", () => editItem(item));
+  return editBtn;
+}
+
+// Locate item
+function locateItem(item) {
+  console.log(`Position of ${item.name}: ${item.position}`);
+}
+
+// Create locate button
+function createLocateButton(item) {
+  const locateBtn = document.createElement("button");
+  locateBtn.innerText = "Locate";
+  locateBtn.addEventListener("click", () => {
+    fetch(`/api/items/${item.id}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({ action: "locate" }),
+    }).catch((error) => console.error(error));
+  });
+  return locateBtn;
+}
+
+// Create item element
+function createItemElement(item) {
+  const li = document.createElement("li");
+  li.dataset.id = item.id;
+
+  const img = document.createElement("img");
+  img.src = item.image;
+  li.appendChild(img);
+
+  const div = document.createElement("div");
+  li.appendChild(div);
+
+  const h2 = document.createElement("h2");
+  h2.innerText = item.name;
+  div.appendChild(h2);
+
+  const p = document.createElement("p");
+  const a = document.createElement("a");
+  a.href = item.link;
+  a.target = "_blank";
+  a.innerText = "Visit Website";
+  p.appendChild(a);
+  div.appendChild(p);
+
+  const span = document.createElement("span");
+  span.innerText = item.position;
+  div.appendChild(span);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.addEventListener("click", () => deleteItem(item.id));
+  div.appendChild(deleteBtn);
+
+  const editBtn = createEditButton(item);
+  div.appendChild(editBtn);
+
+  const locateBtn = createLocateButton(item);
+  div.appendChild(locateBtn);
+
+  return li;
+}
+
+// Load items from server
+function loadItems() {
+  fetch("/api/items")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        const li = createItemElement(item);
+        itemList.appendChild(li);
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
+// Delete item from server and list
+function deleteItem(id) {
+  fetch(`/api/items/${id}`, { method: "DELETE" })
+    .then(() => {
+      const li = itemList.querySelector(`li[data-id="${id}"]`);
+      li.parentNode.removeChild(li);
+    })
+    .catch((error) => console.error(error));
+}
+
+form.addEventListener("submit", addItem);
+loadItems();
---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, jsonify, request
+import csv
+import os
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+DATA_FILE = 'data.csv'
+
+def read_csv():
+    items = []
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, newline='') as f:
+            reader = csv.DictReader(f)
+            items = [row for row in reader]
+    return items
+
+def write_csv(items):
+    with open(DATA_FILE, 'w', newline='') as f:
+        fieldnames = ['id', 'name', 'link', 'image', 'position']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for item in items:
+            writer.writerow(item)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/items', methods=['GET', 'POST'])
+def items():
+    if request.method == 'GET':
+        items = read_csv()
+        return jsonify(items)
+    elif request.method == 'POST':
+        item = request.get_json()
+        items = read_csv()
+        item['id'] = str(len(items) + 1)
+        items.append(item)
+        write_csv(items)
+        return jsonify(item)
+
+
+@app.route('/api/items/<id>', methods=['GET', 'PUT', 'DELETE', 'POST'])
+def item(id):
+    items = read_csv()
+    item = next((item for item in items if item['id'] == id), None)
+    if not item:
+        return jsonify({ 'error': 'Item not found' }), 404
+    if request.method == 'GET':
+        return jsonify(item)
+    elif request.method == 'PUT':
+        item.update(request.get_json())
+        write_csv(items)
+        return jsonify(item)
+    elif request.method == 'DELETE':
+        items = [i for i in items if i['id'] != id]
+        write_csv(items)
+        return jsonify({ 'success': True })
+    elif request.method == 'POST':
+        if request.form.get('action') == 'locate':
+            position = item['position']
+            print(f"Position of {item['name']}: {position}")
+            return jsonify({ 'success': True })
+        else:
+            return jsonify({ 'error': 'Invalid action' }), 400
+
+
+@app.route('/api/items/<id>', methods=['DELETE'])
+def delete_item(id):
+    items = read_csv()
+    items = [item for item in items if item['id'] != id]
+    write_csv(items)
+    return jsonify({ 'success': True })
+
+if __name__ == '__main__':
+    app.run(debug=True)
